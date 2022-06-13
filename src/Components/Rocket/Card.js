e1dabe26e11d1d02b48fb407b30c1dfbffa114b2
jmoseka <jamylam3@gmail.com>
2022-06-13 21:29:49 +0300

    Add card.js

---
+++ src/Components/Rocket/Card.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <div className="card-main">
+      <div className="card">
+        <div className="image">Image</div>
+        <div className="body">
+          <div className="title">Title</div>
+          <div className="text">
+            {`Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+             Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+             when an unknown printer took a galley of type and scrambled it to make a type 
+             specimen book. It has survived not only five centuries,but also the leap into 
+             electronic typesetting, remaining essentially unchanged. It was popularised 
+             in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+             and more recently with desktop publishing software like Aldus PageMaker
+              including versions of Lorem Ipsum.`}
+
+          </div>
+          <div><button className="btnReserveRocket" type="button">Reserve Rocket</button></div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
