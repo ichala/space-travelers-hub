be1f5cb7410565656da84730c86a33b26101edd0
ichala <alijandhd@gmail.com>
2022-06-13 16:14:59 +0100

    Add Api call when page rendered

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import React, { useEffect } from 'react';
 import {
   Routes,
   Route,
 } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Header from './Components/Header/Header';
 import Missions from './Components/Missions/Missions';
+import { FetchMessions, SetMissions } from './Redux/Missions/Missions';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    async function intitData() {
+      const missions = await FetchMessions();
+      dispatch(SetMissions(missions));
+    }
+    intitData();
+  }, []);
   return (
     <>
       <Header />
-
       <hr />
       <div className="content">
         <Routes>
