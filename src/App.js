8c78724c41d9f59a50a6641f5a5e3b05fdb2eae7
ichala <alijandhd@gmail.com>
2022-06-13 13:20:22 +0100

    Add Header Stylings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
+import logo from './images/logo.png';
+
 function App() {
   return (
-    <div />
+    <>
+      <nav>
+        <div className="logo">
+          <img className="logo-header" src={logo} alt="logo" />
+          <h4>Title</h4>
+        </div>
+        <div className="links">
+          <a className="link" href="/"> Missions</a>
+          <a className="link" href="/"> Dragons</a>
+          <a className="link" href="/"> Rockets</a>
+          {' '}
+          |
+          <a className="link" href="/"> Profile</a>
+        </div>
+      </nav>
+      <hr />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<>Home</>} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
