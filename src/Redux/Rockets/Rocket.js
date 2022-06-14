38cddec52aff4ff676cfba12ebfe11d552f63f33
jmoseka <jamylam3@gmail.com>
2022-06-14 11:27:50 +0300

    Modify files

--- src/Redux/Rockets/Rocket.js
+++ src/Redux/Rockets/Rocket.js
@@ -2,11 +2,20 @@ import axios from 'axios';
 
 // action types
 const GET_DATA = 'space-travelers-hub/rockets/rockets';
+const url = 'https://api.spacexdata.com/v3/rockets';
 
 export const getData = (payload) => ({
   type: GET_DATA,
   payload,
 });
+
+export const fetchRocket = () => async () => {
+  const response = await axios.get(url);
+  const data = await response.data;
+  // const rocket= [];
+
+  console.log(data[1]);
+};
 
 export default function rocketReducer(state = [], action) {
   switch (action.type) {
--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducerMission from './Missions/Missions';
+import rocketReducer from './Rockets/Rocket';
 
 const allReducers = combineReducers({
   missions: reducerMission,
+  rockets: rocketReducer,
 });
 const store = createStore(allReducers, applyMiddleware(thunk));
 
