b205d69af5c84159310deabb2024c3dd84cff5ee
jmoseka <jamylam3@gmail.com>
2022-06-14 23:33:07 +0300

    Add function to fetch data in Rocket.js redux

--- src/Redux/Rockets/Rocket.js
+++ src/Redux/Rockets/Rocket.js
@@ -1,25 +1,39 @@
 import axios from 'axios';
 
 // action types
-const GET_DATA = 'space-travelers-hub/rockets/rockets';
+const GET_ROCKET = 'space-travelers-hub/rockets/rockets';
 const url = 'https://api.spacexdata.com/v3/rockets';
 
-export const getData = (payload) => ({
-  type: GET_DATA,
+export const getRockets = (payload) => ({
+  type: GET_ROCKET,
   payload,
 });
 
-export const fetchRocket = () => async () => {
-  const response = await axios.get(url);
-  const data = await response.data;
-  // const rocket= [];
+export const fetchRocket = () => async (dispatch) => {
+  try {
+    const response = await axios.get(url);
+    const data = await response.data;
+    const rocket = [];
 
-  console.log(data[1]);
+    Object.entries(data).forEach((el) => {
+      rocket.push({
+        id: el[1].id,
+        images: el[1].flickr_images[1],
+        rocketName: el[1].rocket_name,
+        description: el[1].description,
+        reserved: false,
+      });
+    });
+
+    dispatch(getRockets(rocket));
+  } catch (error) {
+    <h2>Error</h2>;
+  }
 };
 
 export default function rocketReducer(state = [], action) {
   switch (action.type) {
-    case GET_DATA:
+    case GET_ROCKET:
       return action.payload;
     default:
       return state;
