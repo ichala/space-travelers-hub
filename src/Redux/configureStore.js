d5869fed4403ca2374c55dc560ffa7f58efe1f5d
jmoseka <jamylam3@gmail.com>
2022-06-14 23:49:59 +0300

    Add rocket reducer in configureStore.js

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -2,10 +2,12 @@ import { legacy_createStore as createStore, combineReducers, applyMiddleware } f
 import thunk from 'redux-thunk';
 import reducerMission from './Missions/Missions';
 import dragonReducer from './Dragon/dragon';
+import rocketReducer from './Rocket/rocket';
 
 const allReducers = combineReducers({
   missions: reducerMission,
   dragonReducer,
+  rocketReducer,
 });
 const store = createStore(allReducers, applyMiddleware(thunk));
 
