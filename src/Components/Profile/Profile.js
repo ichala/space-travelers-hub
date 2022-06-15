3dbc9bedf1434cea9fa75ad047626a92f84a8e71
ichala <alijandhd@gmail.com>
2022-06-15 08:59:26 +0100

    Create list in profile

---
+++ src/Components/Profile/Profile.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './profile.css';
+
+function Profile() {
+  return (
+    <div className="profile-content">
+      <div className="profile-missions">
+        <h2>My Missions</h2>
+        <div className="mission-list" >
+            
+        </div>
+      </div>
+      <div className="profile-dragons">2</div>
+      <div className="profile-rockets">3</div>
+    </div>
+  );
+}
+
+export default Profile;
