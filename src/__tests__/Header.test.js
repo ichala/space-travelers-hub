8803c009e0de1cb11dd97fabbdf4671f6277ab61
ichala <alijandhd@gmail.com>
2022-06-15 11:57:15 +0100

    Fix Linters

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,11 +1,10 @@
-
 import { HashRouter as Router } from 'react-router-dom';
-import renderer  from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 import Header from '../Components/Header/Header';
 
-
-it('Header Components Renders correctly',()=>{
-    const tree = renderer.create(
-       <Router><Header /></Router>).toJSON();
-        expect(tree).toMatchSnapshot();
-})
+it('Header Components Renders correctly', () => {
+  const tree = renderer.create(
+    <Router><Header /></Router>,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
--- src/__tests__/Profile.test.js
+++ src/__tests__/Profile.test.js
@@ -1,15 +1,15 @@
-import { Provider } from "react-redux";
-import renderer from "react-test-renderer";
-import Profile from "../Components/Profile/Profile";
-import store from "../Redux/configureStore";
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import Profile from '../Components/Profile/Profile';
+import store from '../Redux/configureStore';
 
-describe("Test of Profile Components", () => {
-  it("Profile Components Renders correctly", () => {
+describe('Test of Profile Components', () => {
+  it('Profile Components Renders correctly', () => {
     const tree = renderer
       .create(
         <Provider store={store}>
           <Profile />
-        </Provider>
+        </Provider>,
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
