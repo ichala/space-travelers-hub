75f307076db562f6e8e2376fc6f8e53515b7f632
jmoseka <jamylam3@gmail.com>
2022-06-15 12:52:22 +0300

    Pass data from rocket file to card

--- src/Components/Rockets/Rocket.js
+++ src/Components/Rockets/Rocket.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+
 import Card from './Card';
 import './Rocket.css';
+import { fetchRocket } from '../../Redux/Rocket/rocket';
 
 function Rocket() {
+  const rockets = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRocket());
+  }, []);
+
   return (
     <div className="card-container">
-      <Card />
-      <Card />
+      {
+        rockets.map((rocket) => (
+          <Card
+            key={rocket.id}
+            rocketList={rocket}
+          />
+
+        ))
+      }
     </div>
   );
 }
